cf00b8513d2c38ce0185b0b5e954975073123ef3
John Bartmann <pravda23@gmail.com>
2026-01-20 16:02:02 +0200

    curl post request working

--- src/config/server.js
+++ src/config/server.js
@@ -1,23 +1,20 @@
 const express = require("express");
 const cors = require("cors");
 const bodyParser = require("body-parser");
+const userRoutes = require("../routes/userRoutes");
 
 const initServer = () => {
   const app = express();
   app.use(cors());
   app.use(bodyParser.json());
+
+  app.get("/", (req, res) => {
+    res.send("Hello world!");
+  });
+
+  app.use("/api/users", userRoutes);
+
   return app;
 };
-
-const app = express();
-const port = 3000;
-
-app.get("/", (req, res) => {
-  res.send("Hello world!");
-});
-
-app.listen(port, () => {
-  console.log(`Example app listening on port ${port}`);
-});
 
 module.exports = initServer;
